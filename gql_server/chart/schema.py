7caed3c666deca79ff6f662bee2687affd90eec2
changhoi <0417kch@naver.com>
2019-09-01 00:48:12 +0900

    finish graphql api

--- gql_server/chart/schema.py
+++ gql_server/chart/schema.py
@@ -48,6 +48,61 @@ AllChartConnectionField = DjangoConnectionField(
 )
 
 
+def can_user_edit_chart(user, target_chart):
+    available_users = target_chart.member + [target_chart.master]
+    if user in available_users:
+        return True
+    else:
+        return False
+
+
+class ChartCellModify(graphene.Mutation):
+    class Arguments:
+        chart_id = graphene.ID()
+        location = graphene.String()
+        text = graphene.String()
+
+    success = graphene.Boolean()
+    chart = graphene.Field(ChartNode)
+
+    def mutate(self, info, chart_id, location, text):
+        # target_chart = Chart.objects.select_related('master').prefetch_related('member').get(id=chart_id)
+        _type, hashid = graphene.relay.Node.from_global_id(chart_id)
+        raw = Chart.objects.get(hashid=hashid)
+        x, y = Chart.get_index(location)
+        data = raw.data
+        data.chart[x].data[y].text = text
+        data.save()
+        # raw._ddb = Noneß
+        return ChartCellModify(success=True, chart=raw)
+        #
+        # if can_user_edit_chart(info.request.user, target_chart):
+        #     x, y = location
+        #     # JSON 내용이 있어야 함
+        #     return ChartCellModify(result="{} cell is modified".format(location), success=True, chart=target_chart)
+        # else:
+        #     return ChartCellModify(result="Not allowed", success=True, chart=target_chart)
+
+
+class ChartNameModify(graphene.Mutation):
+    class Arguments:
+        chart_id = graphene.ID()
+        name = graphene.Argument(graphene.String, description="차트 이름")
+
+    success = graphene.Boolean()
+    chart = graphene.Field(ChartNode)  # 타이틀 수정 => 타이틀만 보내주는 걸로 수정?
+
+    def mutate(self, info, chart_id, name):
+        # target_chart = Chart.objects.select_related('master').prefetch_related('member').get(id=chart_id)
+
+        _type, hashid = graphene.relay.Node.from_global_id(chart_id)
+        raw = Chart.objects.get(hashid=hashid)
+        chart = raw.data
+        chart.name = name
+        chart.save()
+        return ChartNameModify(success=True, chart=raw)
+
+
 class ChartDelete(graphene.Mutation):
     class Arguments:
         chart_id = graphene.ID()
@@ -71,21 +126,26 @@ class ChartCreate(graphene.Mutation):
     chart = graphene.Field(ChartNode)
 
     def mutate(self, info, name):
-        user,_ = User.objects.get_or_create(username="test")
+        user, _ = User.objects.get_or_create(username="test")
         new_chart = Chart.new_chart(user, name)
 
         return ChartCreate(chart=new_chart)
 
 
 class Query(graphene.ObjectType):
-    chart = graphene.Field(ChartNode, description='만다라 차트', question_gid=graphene.ID())
+    chart = graphene.Field(ChartNode, description='만다라 차트', id=graphene.ID())
     all_charts = AllChartConnectionField
 
     def resolve_all_charts(self, info, *args, **kwargs):
         return Chart.objects.all()
 
+    def resolve_chart(self, info, id):
+        _type, hashid = graphene.relay.Node.from_global_id(id)
+        return Chart.objects.filter(hashid=hashid).first()
+
 
 class Mutation(graphene.ObjectType):
     make_chart = ChartCreate.Field()
     delete_chart = ChartDelete.Field()
-
+    modify_chart_cell = ChartCellModify.Field()
+    modify_chart_name = ChartNameModify.Field()
