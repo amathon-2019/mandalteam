0cbbdbc3f1f6ace0cdc70cd01189f5a227d8bb1d
Hakyeon Kim <gkrdus124@naver.com>
2019-08-31 15:48:14 +0900

    차트 제목을 입력할 수 있는 Title 추가
    전역 CSS 변경 (글꼴, 여백)

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,23 @@ const Container = styled.div`
   width: 100%;
 `;
 
+const Title = styled.input`
+  width: 520px;
+  margin: 10px 10px 20px;
+  border: 0;
+  border-bottom: 3px solid #666;
+  font-size: 1.4em;
+  transition: border 0.5s;
+  &:focus {
+    border-bottom: 3px solid #2e92d8;
+    outline: 0;
+  }
+`;
+
 function App() {
   return (
     <Container className="App">
+      <Title />
       <Root />
     </Container>
   );
